1201dd35a2af10dcf4e95a4c409efe8cebe71ce6
matthewheston <matthewheston@gmail.com>
2014-10-16 15:00:21 -0500

    Skeleton main script.

---
+++ yaktrak.py
@@ -0,0 +1,23 @@
+import pyak as pk
+import MySQLdb as mdb
+
+def get_sql_cursor():
+    pass
+
+def get_yaks_for_location(latitude, longitude):
+    pass
+
+def get_comments_for_yak(yak):
+    pass
+
+def persist_comment(comment):
+    pass
+
+def persist_yak(yak):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
