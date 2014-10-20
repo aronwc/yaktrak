0b553e7120ba578a78ac6add75c1d076f3359ad6
matthewheston <matthewheston@gmail.com>
2014-10-20 15:14:29 -0500

    Update table creation scripts for new schema.

--- data/ddl.sql
+++ data/ddl.sql
@@ -2,16 +2,14 @@
 
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `message` longtext,
+  `message` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `message_id` varchar(64) DEFAULT NULL,
-  `latitude` float DEFAULT NULL,
-  `longitude` float DEFAULT NULL,
   `time` timestamp NULL DEFAULT NULL,
   `yak_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `yak_id` (`yak_id`),
   CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`yak_id`) REFERENCES `yaks` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `locations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -19,7 +17,7 @@ CREATE TABLE `locations` (
   `longitude` float DEFAULT NULL,
   `name` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `yakker_ids` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -28,15 +26,14 @@ CREATE TABLE `yakker_ids` (
   PRIMARY KEY (`id`),
   KEY `location_id` (`location_id`),
   CONSTRAINT `yakker_ids_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `yaks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `message` longtext,
+  `message` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `message_id` varchar(64) DEFAULT NULL,
-  `latitude` float DEFAULT NULL,
-  `longitude` float DEFAULT NULL,
   `time` timestamp NULL DEFAULT NULL,
   `score` int(11) DEFAULT NULL,
+  `location_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8;
