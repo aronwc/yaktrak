0060dc6fd0f885a4d1c8714319d1c763cb24a52a
Aron Culotta <aronwc@gmail.com>
2016-02-03 11:13:03 -0600

    add more missing tables/columns

--- data/ddl.sql
+++ data/ddl.sql
@@ -6,6 +6,8 @@ CREATE TABLE `yaks` (
   `message_id` varchar(64) DEFAULT NULL,
   `time` timestamp NULL DEFAULT NULL,
   `score` int(11) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL,
+  `handle` varchar(64) DEFAULT NULL,
   `location_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8;
@@ -38,3 +40,26 @@ CREATE TABLE `yakker_ids` (
   CONSTRAINT `yakker_ids_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE `comment_versions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message_id` varchar(64) DEFAULT NULL,
+  `time_accessed` timestamp NULL DEFAULT NULL,
+  `comment_id` int(11) DEFAULT NULL,
+  `score` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `comment_id` (`comment_id`),
+  CONSTRAINT `comment_versions_ibfk_1` FOREIGN KEY (`comment_id`) REFERENCES `comments` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `yak_versions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message_id` varchar(64) DEFAULT NULL,
+  `time_accessed` timestamp NULL DEFAULT NULL,
+  `yak_id` int(11) DEFAULT NULL,
+  `score` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `yak_id` (`yak_id`),
+  CONSTRAINT `yak_versions_ibfk_1` FOREIGN KEY (`yak_id`) REFERENCES `yaks` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
